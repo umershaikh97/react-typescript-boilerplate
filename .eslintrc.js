cfcbadf6c2c4b46b36c695e7eca22eb7feb6a529
umershaikh97 <umershaikh305@gmail.com>
2022-07-09 02:22:18 +0500

    feat: setup eslint, prettier and husky

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-anonymous-default-export': 0,
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+    'import/named': 'off',
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    '@typescript-eslint/no-namespace': 0,
+    '@typescript-eslint/no-unused-vars': [2, { argsIgnorePattern: '^_' }],
+    'linebreak-style': 'off',
+  },
+};
