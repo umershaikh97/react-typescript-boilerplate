e7b5debd3a60e47cfc97bf8cc0ca0f935717d48c
umershaikh97 <umershaikh305@gmail.com>
2022-07-13 05:10:45 +0500

    chore: update import alias configs

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,8 @@
-const CracoAlias = require('craco-alias');
-
+const path = require('path');
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoAlias,
-      options: {
-        source: 'tsconfig',
-        baseUrl: './src',
-        tsConfigPath: './tsconfig.paths.json',
-      },
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-  ],
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+// import Test from '@/components/Test';
 import './App.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        {/* <Test /> */}
       </header>
     </div>
   );
